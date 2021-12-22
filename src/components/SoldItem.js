cdd77ea6d1997950f90d51bab77806ee3c22edfd
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2021-12-23 07:44:26 +0800

    add SoldItem components + routing

---
+++ src/components/SoldItem.js
@@ -0,0 +1,64 @@
+import { React, useEffect, useState } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from "axios";
+import moment from "moment";
+import classes from "./SoldItem.module.css";
+
+function SoldItem(props) {
+    const [isLoading, setIsLoading] = useState(true);
+    const [soldItem, setSoldItem] = useState([]);
+
+    const navigate = useNavigate();
+
+    const params = useParams();
+    const soldItemId = params.soldItemId ? params.soldItemId : props.id;
+    
+    useEffect(() => {
+        axios.get(`http://localhost:3001/api/soldItems/${soldItemId}`)
+            .then((response) => {
+                setIsLoading(false);
+                setSoldItem(response.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [soldItemId])
+
+    function handleOnClick() {
+        navigate(`/archive/${soldItemId}`);
+    }
+    
+
+    if (isLoading) {
+        return (
+            <div>
+                <p>Loading...</p>
+            </div>
+        )
+    }
+
+    return (
+        <div className={classes.card}>
+            <div className={classes.timestamps}>
+                <span>Created: {moment(soldItem.createdAt).format("MMMM D, YYYY h:mm:ss A")}</span>
+                <span>Updated: {moment(soldItem.updatedAt).format("MMMM D, YYYY h:mm:ss A")}</span>
+            </div>
+            <div className={classes.titleImage}>
+                <span className={classes.title} onClick={handleOnClick}>{soldItem.name}</span>
+                <img className={classes.image} src={soldItem.imageLocation} alt={soldItem.name} />
+            </div>
+            <div className={classes.soldItemDetails}>
+                <div>
+                    <p>Price: P{soldItem.price}</p>
+                    <p>Condition: {soldItem.condition}</p>
+                </div>
+                <div>
+                    <p>Size: {soldItem.size}</p>
+                    <p>Date sold: {moment(soldItem.dateSold).format("MMMM D, YYYY")}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SoldItem;
---
+++ src/components/SoldItemList.js
@@ -0,0 +1,56 @@
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+import SoldItem from "./SoldItem";
+import classes from "./SoldItemList.module.css";
+
+function SoldItemList() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [soldItems, setSoldItems] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/api/soldItems')
+            .then((response) => {
+                setIsLoading(false);
+                setSoldItems(response.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [])
+    
+    if (isLoading) {
+        return (
+            <div className={classes.list}>
+                <p>Loading...</p>
+            </div>
+        )
+    }
+
+    return (
+        <div className={classes.list}>
+            <p>A list of sold items will be shown here...</p>
+            <p>Total items sold: <strong>{soldItems.length}</strong></p>
+            {soldItems.length > 0 ?
+                soldItems.map((soldItem) => (
+                    <SoldItem
+                        key={soldItem.id}
+                        id={soldItem.id}
+                        name={soldItem.name}
+                        price={soldItem.price}
+                        condition={soldItem.condition}
+                        size={soldItem.size}
+                        imageLocation={soldItem.imageLocation}
+                        dateSold={soldItem.dateSold}
+                        createdAt={soldItem.createdAt}
+                        updatedAt={soldItem.updatedAt}
+                    />
+                )) :
+                <div className={classes.noSoldItems}>
+                    You have no sold items at the moment!
+                </div>
+            }
+        </div>
+    )
+}
+
+export default SoldItemList;
