a0d222e7da222c4756b908a32ec56d091bdee437
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2021-12-29 01:18:06 +0800

    add redirect feature if user is not authenticated

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 import classes from "./Profile.module.css";
 
 function Profile() {
     const auth = useSelector((state) => state.auth.value);
+
+    if (auth.bearerToken.length < 1) {
+        console.log(`LOG: Not logged in! Redirecting...`);
+        return <Navigate to='/login' />
+    }
 
     return (
         <div className={classes.main}>
