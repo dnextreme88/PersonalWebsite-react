e02c0206bd5357d1ac63c25ae7c0fa2a7804270f
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2022-01-12 03:14:37 +0800

    refactor user info into its own directory + renamed component

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,15 @@
 import { React } from "react";
-import Profile from "../components/Profile/Profile";
-import classes from "./Profile.module.css";
+import UserInfo from "../components/Profile/UserInfo";
 import PostsByUserLatest from "../components/Blog/PostsByUserLatest";
 import LatestLogins from "../components/Profile/LatestLogins";
+import classes from "./Profile.module.css";
 
 function ProfilePage() {
     return (
         <div>
             <h1 className={classes.title}>Profile Page</h1>
             <hr></hr>
-            <Profile />
+            <UserInfo />
             <LatestLogins />
             <PostsByUserLatest />
         </div>
