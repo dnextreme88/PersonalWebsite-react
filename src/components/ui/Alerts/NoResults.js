10ad9bf3c4e985a63bf0976573665b35edb9bedb
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2022-01-14 09:30:20 +0800

    add alert component for no search results

---
+++ src/components/ui/Alerts/NoResults.js
@@ -0,0 +1,15 @@
+import { Alert } from "react-bootstrap";
+
+function NoResults(props) {
+    return (
+        <Alert variant='danger'>
+            {props.heading ?
+                <Alert.Heading>{props.heading}</Alert.Heading>
+                : ''
+            }
+            {props.message}
+        </Alert>
+    )
+}
+
+export default NoResults;
