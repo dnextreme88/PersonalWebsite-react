2a6effee7253bf61456fe776a4d8440dc8d77126
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2022-03-10 23:08:13 +0800

    add alert for no blog posts

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,6 +2,7 @@ import { React, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Posts from '../components/Blog/Posts'
 import Sidebar from '../components/Blog/Sidebar'
+import NoResults from '../components/ui/Alerts/NoResults'
 import Unauthorized from '../components/ui/Alerts/Unauthorized'
 import Loading from '../components/ui/Spinners/Loading'
 import { SendGetRequest } from '../helpers/SendApiRequest'
@@ -31,7 +32,12 @@ function BlogPage() {
     return (
         <div className={classes.main}>
             <div className={classes.right}><Sidebar /></div>
-            <div className={classes.left}><Posts posts={posts} /></div>
+            <div className={classes.left}>
+                {posts.length > 0 ?
+                    <Posts posts={posts} />
+                    : <NoResults message='You have no blog posts at the moment!' />
+                }
+            </div>
         </div>
     )
 }
