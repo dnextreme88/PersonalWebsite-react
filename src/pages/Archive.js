a73ce276bd34d9d924fb5a43d679d8ece7e78e15
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2023-08-12 00:30:41 +0800

    add pagination for sold items

--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -1,5 +1,6 @@
 import { React, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import ReactPaginate from 'react-paginate'
 import Unauthorized from '../components/ui/Alerts/Unauthorized'
 import Loading from '../components/ui/Spinners/Loading'
 import AddSoldItemForm from '../components/forms/AddSoldItemForm'
@@ -19,11 +20,26 @@ function Archive() {
     const [soldItemsFiltered, setSoldItemsFiltered] = useState([])
     const [isFilter, setIsFilter] = useState(false)
 
+    // Pagination
+    const [itemOffset, setItemOffset] = useState(0)
+    const [pageCount, setPageCount] = useState(0)
+    const [currentPage, setCurrentPage] = useState(0)
+    const [paginatedSoldItems, setPaginatedSoldItems] = useState([])
+    const [paginatedSoldItemsFiltered, setPaginatedSoldItemsFiltered] = useState([])
+
+    const itemsPerPage = 3
+
     useEffect(() => {
         (async function fetchData() {
             const response = await SendGetRequest(auth.bearerToken, 'api/soldItems')
             if (!response.error) {
                 setSoldItems(response)
+
+                const endOffset = itemOffset + itemsPerPage
+                const paginatedItems = response.slice(itemOffset, endOffset)
+                const getPageCount = Math.ceil(response.length / itemsPerPage)
+                setPaginatedSoldItems(paginatedItems)
+                setPageCount(getPageCount)
 
                 // Reset default state so that the page re-renders when a sold item is created or deleted
                 setIsSoldItemCreated(false)
@@ -34,6 +50,30 @@ function Archive() {
             }
         })()
     }, [auth.bearerToken, isSoldItemCreated, isSoldItemDeleted])
+
+    useEffect(() => {
+        (async function paginateData() {
+            const endOffset = itemOffset + itemsPerPage
+            let paginatedItems = ''
+            let getPageCount = 0
+
+            if (!isFilter) {
+                paginatedItems = soldItems.slice(itemOffset, endOffset)
+                setPaginatedSoldItems(paginatedItems)
+
+                getPageCount = Math.ceil(soldItems.length / itemsPerPage)
+            } else {
+                paginatedItems = soldItemsFiltered.slice(itemOffset, endOffset)
+                setPaginatedSoldItemsFiltered(paginatedItems)
+
+                getPageCount = Math.ceil(soldItemsFiltered.length / itemsPerPage)
+            }
+
+            setPageCount(getPageCount)
+
+            console.log(`LOG: Loading items from offset ${itemOffset} to ${endOffset}`)
+        })()
+    }, [itemOffset])
 
     async function handleAddSoldItem(addFormData) {
         const formData = new FormData()
@@ -79,7 +119,24 @@ function Archive() {
         setSoldItemsFiltered(response)
         setIsFilter(true)
 
+        const endOffset = itemOffset + itemsPerPage
+        const paginatedItems = response.slice(itemOffset, endOffset)
+        const getPageCount = Math.ceil(response.length / itemsPerPage)
+        setPaginatedSoldItemsFiltered(paginatedItems)
+        setPageCount(getPageCount)
+        setCurrentPage(0)
+        setItemOffset(0)
+
         setIsLoading(false)
+    }
+
+    async function handlePageClick(event) {
+        const newOffset = (event.selected * itemsPerPage) % soldItems.length
+        const newEndOffset = newOffset + itemsPerPage
+        setCurrentPage(event.selected)
+        setItemOffset(newOffset)
+
+        console.log(`LOG: Current page: ${event.selected + 1}, starting offset (0-based): ${newOffset} - ${newEndOffset}`)
     }
 
     if (isLoading && isAuth) return <Loading />
@@ -101,9 +158,28 @@ function Archive() {
                 : ''
             }
 
+            <ReactPaginate
+                pageCount={pageCount}
+                onPageChange={handlePageClick}
+                forcePage={currentPage}
+                breakLabel="..."
+                nextLabel=">>"
+                previousLabel="<<"
+                pageRangeDisplayed={2}
+                renderOnZeroPageCount={null}
+                containerClassName={'d-flex align-items-center justify-content-center list-unstyled mx-auto'}
+                previousLinkClassName={`text-decoration-none me-2 ${classes.page}`}
+                breakClassName={classes.break}
+                nextLinkClassName={`text-decoration-none ms-2 ${classes.page}`}
+                pageClassName={classes.page}
+                pageLinkClassName={'py-2 px-3 text-decoration-none'}
+                disabledClassName={classes.disabled}
+                activeClassName={'border border-2 border-dark fw-bold'}
+            />
+
             {soldItems.length > 0 && soldItemsFiltered.length === 0 && !isFilter ?
-                <SoldItems soldItems={soldItems} onHandleDeleteSoldItem={handleDeleteSoldItem} /> :
-                <SoldItems soldItems={soldItemsFiltered} onHandleDeleteSoldItem={handleDeleteSoldItem} />
+                <SoldItems soldItems={paginatedSoldItems} onHandleDeleteSoldItem={handleDeleteSoldItem} /> :
+                <SoldItems soldItems={paginatedSoldItemsFiltered} onHandleDeleteSoldItem={handleDeleteSoldItem} />
             }
 
             {/* DISPLAY ERROR MESSAGES */}
